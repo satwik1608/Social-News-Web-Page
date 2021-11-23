dd76fd254c8bfe10ad6ed067d8087ab800fd8159
satwik1608 <satwikkashyap1608@gmail.com>
2021-11-24 00:21:53 +0530

    in progress

--- app.js
+++ app.js
@@ -1,52 +1,8 @@
-let newsList = [
-    // {
-    //     title: "Hacker News",
-    //     URL: "http://news.ycombinator.com",
-    //     author: "Sophie"
-    // },
-    // {
-    //     title: "Reddit",
-    //     URL: "https://reddit.com",
-    //     author: "Thomas"
-    // },
-    // {
-    //     title: "BoingBoing",
-    //     URL: "http://boingboing.net",
-    //     author: "Daniel"
-    // },
-
-];
-
 fetch("https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=659bf7b6e5ec435b8d70b022cd25b125")
 .then(response => response.json())
 .then(headline =>{
     headline.articles.forEach(val =>{
-        const hero = document.createElement('div');
-        const titleElement = document.createElement('div');
-        const linkElement = document.createElement('a');
-        const authorElement = document.createElement('div');
-        
-        linkElement.textContent = val.url;
-        linkElement.setAttribute('href', val.url);
-        titleElement.textContent=val.title;
-        
-        authorElement.textContent=`Submitted by ${val.author}`;
-    
-    
-        titleElement.classList.add('titleElement');
-        linkElement.classList.add('linkElement');
-        authorElement.classList.add('authorElement');
-    
-    
-        hero.classList.add('hero');
-    
-    
-        hero.appendChild(titleElement);
-        hero.appendChild(linkElement);
-        hero.appendChild(authorElement);
-    
-    
-        showData.appendChild(hero);
+        showUpdated(val.url,val.title,val.author);
     });
      
     
@@ -56,20 +12,18 @@ fetch("https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=659bf7b6e5ec
 const showData = document.getElementById('showData');
 // showUpdated();
 
-function showUpdated(){
-    for(var i =0;i<newsList.length;++i){
-
+function showUpdated(url,title,author){
 
     const hero = document.createElement('div');
     const titleElement = document.createElement('div');
     const linkElement = document.createElement('a');
     const authorElement = document.createElement('div');
     
-    linkElement.textContent = newsList[i].URL;
-    linkElement.setAttribute('href', newsList[i].URL);
-    titleElement.textContent=newsList[i].title;
+    linkElement.textContent = url;
+    linkElement.setAttribute('href', url);
+    titleElement.textContent=title;
     
-    authorElement.textContent=`Submitted by ${newsList[i].author}`;
+    authorElement.textContent=`Submitted by ${author}`;
 
 
     titleElement.classList.add('titleElement');
@@ -88,7 +42,7 @@ function showUpdated(){
     showData.appendChild(hero);
 
 
-}
+
 };
 
 
@@ -103,6 +57,32 @@ const required = document.getElementById('dataRequired');
 
 const successDiv = document.getElementById('success');
 
+const addRandom = document.getElementById('submit2');
+
+addRandom.addEventListener('click', ()=>{
+    fetch("https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=659bf7b6e5ec435b8d70b022cd25b125")
+    .then(response => response.json())
+    .then(headline =>{
+        const val = headline.articles[0];
+            showUpdated(val.url,val.title,val.author);
+            const success = () =>{
+                const messages = document.createElement('p');
+                messages.textContent =`The link ${val.title} has been succesfully added!`;
+                successDiv.appendChild(messages);
+                setTimeout(()=>{
+                    successDiv.removeChild(messages);
+                }, 3000);
+            }
+            success();
+    successDiv.classList.remove('hideSuccess');
+    setTimeout(() =>{
+        successDiv.classList.add('hideSuccess'); 
+    }, 2000);
+            
+    
+    })  
+});
+
 submit.addEventListener('click', ()=>{
     addData.classList.remove('hideAdd');
 });
@@ -111,65 +91,32 @@ submit.addEventListener('click', ()=>{
 
 addLink.addEventListener('click', ()=>{
 
-    // var temp = {
-    //     author: "",
-    //     URL : "",
-    //     title : ""
-    // };
-
     var temp2 = inputLink.value;
     
     var temp3= inputAuthor.value;
     var temp4 = inputTitle.value;
     console.log(temp2);
 
-    // if(temp2===""||temp3===""||temp2===""){
-    //     if(required.classList.contains('hideRequired')){
-    //     required.classList.remove('hideRequired');
-    //     }
-    // }
+    if(temp2===""||temp3===""||temp2===""){
+        if(required.classList.contains('hideRequired')){
+        required.classList.remove('hideRequired');
+        }
+    }
 
-    // else{
-    //     if(!required.classList.contains('hideRequired')){
-    //         required.classList.add('hideRequired');
-    //     }
+    else{
+        if(!required.classList.contains('hideRequired')){
+            required.classList.add('hideRequired');
+        }
     
-    // if(temp2.startsWith!="https://"||temp2.startsWith!="http://"){
-    // const flag="http://"
-    // temp2=flag.concat(temp2);
-    // }
-
-    const hero = document.createElement('div');
-    const titleElement = document.createElement('div');
-    const linkElement = document.createElement('a');
-    const authorElement = document.createElement('div');
-    
-    linkElement.textContent = temp2;
-    linkElement.setAttribute('href', temp2);
-    titleElement.textContent=temp4;
-    
-    authorElement.textContent=`Submitted by ${temp3}`;
-
-
-    titleElement.classList.add('titleElement');
-    linkElement.classList.add('linkElement');
-    authorElement.classList.add('authorElement');
-
-
-    hero.classList.add('hero');
-
-
-    hero.appendChild(titleElement);
-    hero.appendChild(linkElement);
-    hero.appendChild(authorElement);
-
-
-    showData.appendChild(hero);
+        if(temp2.startsWith!="https://"||temp2.startsWith!="http://"){
+            const flag="http://"
+            temp2=flag.concat(temp2);
+        }
     
 
-    // var tempTitle = inputTitle.value;
+    
 
-    // newsList.unshift(temp);
+    showUpdated(temp2,temp4,temp3);
 
     const success = () =>{
         const messages = document.createElement('p');
@@ -183,8 +130,6 @@ addLink.addEventListener('click', ()=>{
     inputLink.value="";
     inputAuthor.value="";
     inputTitle.value="";
-    // showData.innerText="";
-    // showUpdated();
     addData.classList.add('hideAdd');
     success();
     successDiv.classList.remove('hideSuccess');
@@ -192,7 +137,7 @@ addLink.addEventListener('click', ()=>{
         successDiv.classList.add('hideSuccess'); 
     }, 2000);
 
-
+    }
 
 });
 
